45f078194a54a78780421ebfd70588194891e9b3
benop7 <benjaminpetegnong@gmail.com>
2023-04-17 18:06:49 +0100

    Initial setup

---
+++ app/src/main/java/com/glatec/hymnal_android_gla/MainActivity.java
@@ -0,0 +1,14 @@
+package com.glatec.hymnal_android_gla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
