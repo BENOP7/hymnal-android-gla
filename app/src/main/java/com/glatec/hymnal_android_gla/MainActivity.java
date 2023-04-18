d05df0dfe2f18cfebad90c5ed07c38282c554688
benop7 <benjaminpetegnong@gmail.com>
2023-04-18 21:59:28 +0100

    design navigation drawer and welcome screen

--- app/src/main/java/com/glatec/hymnal_android_gla/MainActivity.java
+++ app/src/main/java/com/glatec/hymnal_android_gla/MainActivity.java
@@ -1,14 +1,29 @@
 package com.glatec.hymnal_android_gla;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer,
+                R.string.Open_drawer_text, R.string.Close_drawer_text);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
---
+++ app/src/main/java/com/glatec/hymnal_android_gla/WelcomeActivity.java
@@ -0,0 +1,29 @@
+package com.glatec.hymnal_android_gla;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        btn = findViewById(R.id.fr_opt_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
