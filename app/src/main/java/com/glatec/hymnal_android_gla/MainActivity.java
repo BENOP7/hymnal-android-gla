47ef1045c028b316e3b3356b5ed40261dfcbb1b6
benop7 <benjaminpetegnong@gmail.com>
2023-04-21 09:04:26 +0100

    Setup recycler view fragment

--- app/src/main/java/com/glatec/hymnal_android_gla/MainActivity.java
+++ app/src/main/java/com/glatec/hymnal_android_gla/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawer;
     private Toolbar toolbar;
+    private HymnFragment hymnFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +27,15 @@ public class MainActivity extends AppCompatActivity {
                 R.string.Open_drawer_text, R.string.Close_drawer_text);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        hymnFragment = new HymnFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, hymnFragment)
+                .commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
